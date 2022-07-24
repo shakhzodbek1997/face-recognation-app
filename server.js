6e68a4382c945ebc36f1831566b63f0284a24c0e
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-07-24 09:14:39 +0200

    fix 3

--- server.js
+++ server.js
@@ -22,15 +22,20 @@ const db = knex({
 const app = express();
 
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(bodyParser.json()); // app.use(bodyparser.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res) => {res.send('it is working')})
+
 app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.port || 3000, ()=> {
-  console.log('app is running on port 3000');
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
